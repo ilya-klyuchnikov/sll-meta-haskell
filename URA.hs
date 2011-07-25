6e281073e0f41252a9e5a4b164cd9b8cf4d89937
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2011-07-25 12:14:10 +0400

    URA is simplified: it is enough to use just one pending list for breadth-first traversals

--- URA.hs
+++ URA.hs
@@ -5,14 +5,13 @@ import DataUtil
 import Driving
 
 ura :: Machine Conf -> Conf -> Expr -> [Subst Conf]
-ura machine conf answer = traverse [(idContr conf, buildConfTree machine conf)] [] where
-    traverse :: [(Subst Conf, Tree Conf)] -> [(Subst Conf, Tree Conf)] -> [Subst Conf]
-    traverse [] [] = []
-    traverse [] queue' = traverse queue' []
-    traverse ((sub, t) : queue) queue' =
+ura machine conf answer = traverse [(idContr conf, buildConfTree machine conf)] where
+    traverse :: [(Subst Conf, Tree Conf)] -> [Subst Conf]
+    traverse [] = []
+    traverse ((sub, t) : queue) =
         case t of
-            Leaf a | a == answer -> sub : traverse queue queue'
-            Leaf a | otherwise   -> traverse queue queue'
-            Node _ (ETransient _ t') -> traverse queue ((sub, t') : queue')
-            Node _ (EVariants variants) -> traverse queue (queue'' ++ queue') where
-                queue'' = map (\(contr, t') -> (sub /// contr, t')) variants
+            Leaf a | a == answer -> sub : traverse queue
+            Leaf a | otherwise   -> traverse queue
+            Node _ (ETransient _ t') -> traverse (queue ++ [(sub, t')])
+            Node _ (EVariants variants) -> traverse (queue ++ queue') where
+                queue' = map (\(contr, t') -> (sub /// contr, t')) variants
