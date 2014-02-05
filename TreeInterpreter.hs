9936938cfa27777197e9bfde0c7d5023e5f872a7
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-02-05 10:11:55 +0400

    Extending of TreeInterpreter to work with if-expressions

--- TreeInterpreter.hs
+++ TreeInterpreter.hs
@@ -4,8 +4,11 @@ import Data
 import Data.List
 import DataUtil
 import Data.Maybe
+import DataIO
 
 -- is able to interprete a tree of configuration, given an environment
+-- assumptions - NO if-expressions
+-- this is an interpreter for MIXED "semantics"
 intTree :: Tree Expr -> Env -> Expr
 intTree (Leaf e) env =
     e // env
@@ -17,7 +20,13 @@ intTree (Node e (EVariants cs)) env =
     head $ catMaybes $ map (try env) cs
 
 try :: Env -> (Subst Expr, Tree Expr) -> (Maybe Expr)
+-- outcome of matching
 try env ([(v, ctr@(Ctr pn _))], t) =
     if cn == pn then (Just $ intTree t extEnv) else Nothing where
+    	-- evaluate a scrutenee var in current environment
         c@(Ctr cn cargs) = (Var v []) // env
+        -- extending environment
         extEnv = zip (vnames ctr) cargs ++ env
+-- outcome of if-expression
+try env ((v, e2):_, t) =
+	if ((Var v [] // env) == (e2 // env)) then (Just $ intTree t env) else Nothing
