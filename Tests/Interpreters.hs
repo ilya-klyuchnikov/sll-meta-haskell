3a3e6cd1dc2f7be9fafe77cba566570516852245
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-02-10 09:36:54 +0400

    grouping tests by interpreters

--- Tests/Interpreters.hs
+++ Tests/Interpreters.hs
@@ -4,8 +4,6 @@ module Tests.Interpreters (intTests) where
 -- there are tests of two different kinds:
 -- 1) testing by example (input - output)
 -- 2) testing subtleties of semantics (that some interpreters produce errors)
--- TODO - since all (4) interpreters for each semantics should have the same input/output
--- we may group tests
 
 import Test.HUnit
 import Control.Exception
@@ -14,90 +12,69 @@ import Examples.Examples
 import Data
 import DataIO
 
-import qualified Interpreters.BigStepCBV as IntBigStepCBV
 import qualified Interpreters.BigStepCBN as IntBigStepCBN
-import qualified Interpreters.SmallStepCBV as IntSmallStepCBV
 import qualified Interpreters.SmallStepCBN as IntSmallStepCBN
-
 import qualified Interpreters.StagedSmallStepCBN as IntStagedSmallStepCBN
-import qualified Interpreters.StagedSmallStepCBV as IntStagedSmallStepCBV
 import qualified Interpreters.StagedMixedStepCBN as IntStagedMixedStepCBN
+
+import qualified Interpreters.BigStepCBV as IntBigStepCBV
+import qualified Interpreters.SmallStepCBV as IntSmallStepCBV
+import qualified Interpreters.StagedSmallStepCBV as IntStagedSmallStepCBV
 import qualified Interpreters.StagedMixedStepCBV as IntStagedMixedStepCBV
 
 type Interpreter = Program -> Expr -> Expr
 
-tryAny :: IO a -> IO (Either SomeException a)
-tryAny = try
+callByValueInterpreters =
+    [IntBigStepCBV.int, IntSmallStepCBV.int, IntStagedSmallStepCBV.int, IntStagedMixedStepCBV.int]
+callByNameInterpreters  =
+    [IntBigStepCBN.int, IntSmallStepCBN.int, IntStagedSmallStepCBN.int, IntStagedMixedStepCBN.int]
 
 -- checks input/output relation for execution
-testInt :: String -> Interpreter -> Program -> String -> String -> Test
-testInt name int prog input output = TestCase $ assertEqual
+assertIntInputOutput :: String -> Interpreter -> Program -> String -> String -> Test
+assertIntInputOutput name int prog input output = TestCase $ assertEqual
 	name
 	(read output)
 	(int prog (read input))
 
 -- checks that execution by a given interpreter result in an error
-assertIntError :: (Program -> Expr -> Expr) -> Program -> String -> Test
+assertIntError :: Interpreter -> Program -> String -> Test
 assertIntError int prog input = TestCase $ do
 	result <- tryAny (evaluate (int prog (read input)))
 	case result of
 		Left _ -> return ()
 		Right _ -> assertFailure $ "Expected exception: "
+    where
+        -- catching all exceptions
+        tryAny :: IO a -> IO (Either SomeException a)
+        tryAny = try
+
+assertCallByNameIntInputOutput :: String -> Program -> String -> String -> Test
+assertCallByNameIntInputOutput name program input output =
+    TestList [assertIntInputOutput name int program input output | int <- callByNameInterpreters]
+
+assertCallByValueIntInputOutput :: String -> Program -> String -> String -> Test
+assertCallByValueIntInputOutput name program input output =
+    TestList [assertIntInputOutput name int program input output | int <- callByValueInterpreters]
+
+assertCallByNameIntError :: String -> Program -> String -> Test
+assertCallByNameIntError name program input =
+    TestList [assertIntError int program input | int <- callByNameInterpreters]
+
+assertCallByValueIntError :: String -> Program -> String -> Test
+assertCallByValueIntError name program input =
+    TestList [assertIntError int program input | int <- callByValueInterpreters]
 
 -- tests that CBV execution always evals arguments
 -- since one argument is illegal (calling non-existing function)
 -- CBV execution should result in error
-testSSCBV1 =
-	assertIntError IntSmallStepCBV.int progTests "fFstArg(Z(), fError(Z()))"
-testSSCBV2 =
-	assertIntError IntSmallStepCBV.int progTests "fSndArg(fError(Z()), Z())"
-
-testBSCBV1 =
-	assertIntError IntBigStepCBV.int progTests "fFstArg(Z(), fError(Z()))"
-testBSCBV2 =
-	assertIntError IntBigStepCBV.int progTests "fSndArg(fError(Z()), Z())"
-
-testSSSCBV1 =
-    assertIntError IntStagedSmallStepCBV.int progTests "fFstArg(Z(), fError(Z()))"
-testSSSCBV2 =
-    assertIntError IntStagedSmallStepCBV.int progTests "fSndArg(fError(Z()), Z())"
-
-testSMSCBV1 =
-    assertIntError IntStagedMixedStepCBV.int progTests "fFstArg(Z(), fError(Z()))"
-testSMSCBV2 =
-    assertIntError IntStagedMixedStepCBV.int progTests "fSndArg(fError(Z()), Z())"
+testCBV1 = assertCallByValueIntError "testCBV1" progTests "fFstArg(Z(), fError(Z()))"
+testCBV2 = assertCallByValueIntError "testCBV2" progTests "fSndArg(fError(Z()), Z())"
 
 -- CNB doesn't touch arguments
-testSSCBN1 =
-	testInt "test1" IntSmallStepCBN.int progTests "fFstArg(Z(), fError(Z()))" "Z()"
-testSSCBN2 =
-	testInt "test2" IntSmallStepCBN.int progTests "fSndArg(fError(Z()), Z())" "Z()"
-
-testBSCBN1 =
-    testInt "test1" IntBigStepCBN.int progTests "fFstArg(Z(), fError(Z()))" "Z()"
-testBSCBN2 =
-    testInt "test2" IntBigStepCBN.int progTests "fSndArg(fError(Z()), Z())" "Z()"
-
-testSSSCBN1 =
-    testInt "test1" IntStagedSmallStepCBN.int progTests "fFstArg(Z(), fError(Z()))" "Z()"
-testSSSCBN2 =
-    testInt "test2" IntStagedSmallStepCBN.int progTests "fSndArg(fError(Z()), Z())" "Z()"
-
-testSMSCBN1 =
-    testInt "test1" IntStagedMixedStepCBN.int progTests "fFstArg(Z(), fError(Z()))" "Z()"
-testSMSCBN2 =
-    testInt "test2" IntStagedMixedStepCBN.int progTests "fSndArg(fError(Z()), Z())" "Z()"
+testCBN1 = assertCallByNameIntInputOutput "testCBN1" progTests "fFstArg(Z(), fError(Z()))" "Z()"
+testCBN2 = assertCallByNameIntInputOutput "testCBN2" progTests "fSndArg(fError(Z()), Z())" "Z()"
 
 
-intTests = TestList [
-                    testSSCBV1, testSSCBV2,
-                    testBSCBV1, testBSCBV2,
-                    testSSSCBV1, testSSSCBV2,
-                    testSMSCBV1, testSMSCBV2,
-                    testSSCBN1, testSSCBN2,
-                    testBSCBN1, testBSCBN2,
-                    testSSSCBN1, testSSSCBN2,
-                    testSMSCBN1, testSMSCBN2
-                    ]
+intTests = TestList [testCBN1, testCBN2, testCBV1, testCBV2]
 
 main = runTestTT intTests
